230071f16a79ab5a4cb00992130eb0d939bb4e0a
XeGrox <xegrox@protonmail.com>
2021-06-23 12:19:03 +0800

    refactor(modal): teleport modal to body

--- client/src/components/ui/Modal.vue
+++ client/src/components/ui/Modal.vue
@@ -1,11 +1,15 @@
 <template>
-  <FadeTransition>
-    <div v-show="show" class="absolute w-full h-full z-20 bg-black bg-opacity-50">
-      <div :style="{ 'width': width, 'height': height }" class="p-8 bg-gray-800 shadow-md hover:shadow-lg rounded-2xl absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 p-10">
-        <slot/>
-      </div>
-    </div>
-  </FadeTransition>
+  <div>
+    <teleport to="body">
+      <FadeTransition>
+        <div v-show="show" class="absolute top-0 left-0 w-full h-full z-20 bg-black bg-opacity-50">
+          <div :style="{ 'width': width, 'height': height }" class="p-8 bg-gray-800 shadow-md hover:shadow-lg rounded-2xl absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 p-10">
+            <slot/>
+          </div>
+        </div>
+      </FadeTransition>
+    </teleport>
+  </div>
 </template>
 
 <script lang="ts">
