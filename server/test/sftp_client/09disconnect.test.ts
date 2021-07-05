b1bf8b15a20c9e7053e3deffde9c5a2a0c93c5cb
XeGrox <xegrox@protonmail.com>
2021-07-05 13:37:59 +0800

    test(server): remove unnecessary connection test in 09disconnect

--- server/test/sftp_client/09disconnect.test.ts
+++ server/test/sftp_client/09disconnect.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
-import { sendRpcRequest, sftpErrorCode } from './utils'
+import { sendRpcRequest } from './utils'
 import { newConnection } from './hooks/global-hooks'
-import { SuccessObject, ErrorObject } from 'jsonrpc-lite'
+import { SuccessObject } from 'jsonrpc-lite'
 
 let sessionId: string
 
@@ -11,17 +11,9 @@ describe('COMMAND disconnect', () => {
     sessionId = await newConnection()
   })
 
-  it('When_HaveConnection_Should_ReturnSuccess_[void]Disconnected', async () => {
+  it('When_HaveConnection_Should_ReturnSuccess_[null]Disconnected', async () => {
     let payload = await sendRpcRequest('disconnect', [], sessionId)
     expect(payload).to.be.instanceOf(SuccessObject)
-  })
-
-  it('When_NoConnection_Should_ReturnError_[-32000, ERR_NOT_CONNECTED]NotConnected', async () => {
-    let payload = await sendRpcRequest('disconnect', [], sessionId)
-    expect(payload).to.be.instanceOf(ErrorObject)
-    payload = payload as ErrorObject
-    let err = payload.error.data as NodeJS.ErrnoException
-    expect(err.code, sftpErrorCode.connect)
   })
 
 })
