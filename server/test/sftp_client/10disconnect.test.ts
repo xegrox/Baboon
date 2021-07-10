c754f22db00de4f589e15ba4aa4bad15fa27378f
XeGrox <xegrox@protonmail.com>
2021-07-10 15:44:09 +0800

    test(server): add params test in 10disconnect

--- server/test/sftp_client/10disconnect.test.ts
+++ server/test/sftp_client/10disconnect.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { sendRpcRequest } from './utils'
+import { testRpcRequestParams, sendRpcRequest } from './utils'
 import { newConnection } from './hooks/global-hooks'
 import { SuccessObject } from 'jsonrpc-lite'
 
@@ -10,6 +10,8 @@ describe('COMMAND disconnect', () => {
   before(async () =>  {
     sessionId = await newConnection()
   })
+
+  it('When_RequiredParamsPassed_Should_NotThrowError', () => testRpcRequestParams('disconnect', {}))
 
   it('When_HaveConnection_Should_ReturnSuccess_[null]Disconnected', async () => {
     let payload = await sendRpcRequest('disconnect', undefined, sessionId)
